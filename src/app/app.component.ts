7ba5275c80006eaebb8ef44405419414b61bd833
darc182 <darwin.dc182@gmail.com>
2025-05-06 16:59:33 -0500

    segundo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonDatetime } from '@ionic/angular/standalone';
+import { IonLabel,IonItem } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet,IonDatetime,IonLabel,IonItem],
 })
 export class AppComponent {
   constructor() {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,34 +5,34 @@
 export const environment = {
   production: false,
   // URL de la API
-  firebaseConfig : {
+  // firebaseConfig : {
 
-    apiKey: "AIzaSyApmLZbwwzkWHaMefftXLnoeCrtrqWwe38",
+  //   apiKey: "AIzaSyApmLZbwwzkWHaMefftXLnoeCrtrqWwe38",
   
-   authDomain: "demomovil-856fc.firebaseapp.com",
+  //  authDomain: "demomovil-856fc.firebaseapp.com",
   
-    projectId: "demomovil-856fc",
+  //   projectId: "demomovil-856fc",
   
-   storageBucket: "demomovil-856fc.firebasestorage.app",
+  //  storageBucket: "demomovil-856fc.firebasestorage.app",
   
-     messagingSenderId: "125390567669",
+  //    messagingSenderId: "125390567669",
   
-     appId: "1:125390567669:web:67fb426273259005912dbc",
+  //    appId: "1:125390567669:web:67fb426273259005912dbc",
   
-     measurementId: "G-MQPGFC6F7B"
+  //    measurementId: "G-MQPGFC6F7B"
   
-   }
+  //  }
 
-//    firebaseConfig : {
-//     apiKey: "AIzaSyByQgiCUzf3gYfFYcw4Pl7Rec-yRFjSIZA",
-//     authDomain: "a-e2689.firebaseapp.com",
-//     projectId: "a-e2689",
-//     storageBucket: "a-e2689.firebasestorage.app",
-//     messagingSenderId: "941773529283",
-//     appId: "1:941773529283:web:8a9199b937e0fe88a88276",
-//     measurementId: "G-PB8QCM0HKG"
+    firebaseConfig : {
+     apiKey: "AIzaSyByQgiCUzf3gYfFYcw4Pl7Rec-yRFjSIZA",
+     authDomain: "a-e2689.firebaseapp.com",
+     projectId: "a-e2689",
+     storageBucket: "a-e2689.firebasestorage.app",
+     messagingSenderId: "941773529283",
+     appId: "1:941773529283:web:8a9199b937e0fe88a88276",
+     measurementId: "G-PB8QCM0HKG"
   
-// }
+ }
 }
 
 /*
